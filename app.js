5e4b8e5b381e67d4289f54b5ece0c2bb7419426d
muluken-beep <60249295+muluken-beep@users.noreply.github.com>
2021-07-26 08:28:14 -0700

    still

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+const decrease = document.querySelector(".decrease");
+const reset = document.querySelector('.reset');
+const increase = document.querySelector(".increase");
+
+
+decrease.addEventListener("click", function (){
+let val = document.getElementById('value');
+val.innerHTML -= 1;
+val.style.color = 'red';
+});
+
+
+reset.addEventListener("click", function (){
+  let val =document.getElementById('value');
+  val.innerHTML = 0;
+  val.style.color = 'black';
+})
+
+
+
+increase.addEventListener("click", function (){
+let val = document.getElementById('value');
+let valInt = Number(val.innerHTML);
+valInt += 1
+val.style.color = 'green';
+});
