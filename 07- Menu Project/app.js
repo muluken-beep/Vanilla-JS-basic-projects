7e8076b0e7c4f7ee7aa46787ac4940862f7f6c6b
muluken-beep <60249295+muluken-beep@users.noreply.github.com>
2021-07-30 07:26:28 -0700

    completed Menu project

--- 07- Menu Project/app.js
+++ 07- Menu Project/app.js
@@ -76,33 +76,14 @@ const menu = [
 
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn");
+const container = document.querySelector(".btn-container")
+
 
 
 // Load items
-
 window.addEventListener('DOMContentLoaded', function(){
   displayMenuItems(menu);
- });
-
-//filter items
- filterBtns.forEach(function(btn){
-   btn.addEventListener("click", function(e){
-     const category = e.currentTarget.dataset.id;
-     const menuCategory= menu.filter(function (menuItem){
-
-      if (menuItem.category ===category){
-        return menuItem;
-      }
-     });
-    //  console.log(menuCategory);
-    if (menuItem.category ==="all"){
-      displayMenuItems(menu);
-    }
-    else{
-      displayMenuItems(menuCategory);
-    }
-   });
+  displayMenuButtons();
  });
 
 
@@ -121,4 +102,45 @@ function displayMenuItems(menuItems){
     });
     displayMenu = displayMenu.join("")
     sectionCenter.innerHTML = displayMenu;
+}
+
+
+
+function displayMenuButtons(){
+  const categories = menu.reduce( 
+    function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    };    
+    return values;
+   }, ["all"]);  
+
+  const categoryBtns = categories.map(
+    function(category){
+    return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+  }).join("")
+  // console.log(categoryBtns);
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+   
+//filter items
+ filterBtns.forEach(function(btn){
+  btn.addEventListener("click", function(e){
+    const category = e.currentTarget.dataset.id;
+    const menuCategory= menu.filter(function (menuItem){
+     // console.log(menuItem.category);
+
+    if (menuItem.category === category){
+       return menuItem;
+     }
+     });     
+   //  console.log(menuCategory);
+   if (category ==="all"){
+     displayMenuItems(menu);
+   }
+   else{
+     displayMenuItems(menuCategory);
+   }
+  });
+});
 }
